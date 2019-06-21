bb8c52cb09143fb9845f78ae74395acdd977bb24
liupu <575866447@qq.com>
2019-06-21 15:11:19 +0800

    Hook test

--- app/src/main/java/com/update/hookams/MainActivity.java
+++ app/src/main/java/com/update/hookams/MainActivity.java
@@ -1,13 +1,36 @@
 package com.update.hookams;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.update.hookams.hook.HookHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 这个方法比onCreate调用早; 在这里Hook比较好.
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        HookHelper.hookActivityManager();
+        super.attachBaseContext(newBase);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv_hello).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 测试AMS HOOK (调用其相关方法)
+                Uri uri = Uri.parse("http://wwww.baidu.com");
+                Intent t = new Intent(Intent.ACTION_VIEW);
+                t.setData(uri);
+                startActivity(t);
+            }
+        });
     }
 }
